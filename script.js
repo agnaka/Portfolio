966a98c7791eaaa4c19d28a19c9b17b627880978
agnaka <agn.muleto@gmail.com>
2024-09-28 22:00:28 -0300

    Semântica e acessibilidade

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function scrollToTop() {
   // Função para mostrar o botão quando o usuário rolar para baixo
   window.addEventListener('scroll', () => {                 //Adiciona um ouvinte de eventos ao objeto window. Isso significa que toda vez que o usuário rolar a página, a função dentro do addEventListener será executada.
     const btn = document.querySelector('.btn-voltar-topo'); //Seleciona o elemento HTML com a classe btn-voltar-topo (nosso botão) e armazena-o na constante btn
-    if (window.scrollY > 200) {                             //Verifica se a quantidade de pixels que o usuário rolou para baixo é maior que 100. Se sim, significa que o usuário já rolou uma distância considerável.
+    if (window.scrollY > 200) {                             //Verifica se a quantidade de pixels que o usuário rolou para baixo é maior que 200. Se sim, significa que o usuário já rolou uma distância considerável.
       btn.classList.add('show');                            // Adiciona a classe show ao botão. Essa classe, no CSS, deve tornar o botão visível (por exemplo, alterando a opacidade de 0 para 1)
     } else {
       btn.classList.remove('show');                         //Remove a classe show do botão. Isso torna o botão invisível novamente.
